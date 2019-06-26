19ff56ddb5656cc1dfd41f0c3189f88657e4b833
Jake Pilikian <ja.pilikian@gmail.com>
2019-06-26 17:43:29 +0100

    add a function to increase and decrease temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,6 +9,17 @@ describe('Thermostat', function() {
   });
 
     it('starts at 20 degrees', function() {
-    expect(thermostat.temp).toEqual(20);
+    expect(thermostat.currentTemp()).toEqual(20);
   });
+
+  it('increases temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.currentTemp()).toEqual(21);
+  });
+
+  it('decreases temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.currentTemp()).toEqual(19);
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -4,3 +4,17 @@ function Thermostat() {
    this.temp = 20;
 
    }
+
+Thermostat.prototype.currentTemp = function() {
+  return this.temp;
+  };
+
+Thermostat.prototype.up = function() {
+    this.temp += 1;
+
+  }
+
+Thermostat.prototype.down = function() {
+      this.temp -= 1;
+
+  }
